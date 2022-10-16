125128ee8d679411f085d249adb1d89ebd29ff93
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2022-10-16 17:16:51 -0300

    Finalização do Projeto Final; Object Prototype; RegExp; Google Charts

--- projeto4/formulario.js
+++ projeto4/formulario.js
@@ -13,6 +13,63 @@ document.querySelector("#sucesso").addEventListener('hidden.bs.modal', () => {
     window.location.href = './index.html'
 })
 
+
+// Acessando link https://example.com/?nome=Wesley&sobrenome=Oliveira&idade=24
+const urlParams = new URLSearchParams(window.location.search)
+
+const nome = urlParams.get("nome")
+console.log(nome) // Exibe "Wesley"
+
+const sobrenome = urlParams.get("sobrenome")
+console.log(sobrenome) // Exibe "Oliveira"
+
+const idade = Number(urlParams.get("idade"))
+console.log(idade) // Exibe 24
+
+
+
+var usuario = false
+
+window.addEventListener("load", () => {
+    //Identifica se possui o id como URL Params
+    let id = new URLSearchParams(window.location.search).get("id")
+    if (!id) return
+
+    //Exibe o loading
+    document.querySelector(".container").classList.add("oculto")
+    document.querySelector("#loading").classList.remove("oculto")
+
+    //Faz a requisição via fetch enviando a nossa credencial em base64 no header
+    //E passa o id recebido pela query param
+    fetch("https://api-curso-programacao-web.vercel.app/api/usuarios/"+id, {
+        headers: {
+            "Authorization": "Basic " + btoa('admin:admin') //converte em base64
+        }
+    })
+        .then(resposta => resposta.json()) // converte a resposta e, json
+        .then(resposta => {
+            console.log(resposta)
+            //chama a função de montar a tabela com os dados em json
+            usuario = resposta
+            preencheForm(resposta)
+        })
+        .catch(error => {
+            console.log(error.message)
+            //caso haja um erro na requisição, o modal é exibido
+            modal.show()
+        })
+})
+
+function preencheForm(resposta) {
+    let campos = Object.keys(resposta)
+    campos.forEach(campo => {
+        if (campo !== "id") document.querySelector("#"+campo).value = resposta[campo]
+    })
+
+    document.querySelector(".container").classList.remove("oculto")
+    document.querySelector("#loading").classList.add("oculto")
+}
+
 document.querySelector("#cep").addEventListener("input", event => {
     //Adiciona o loading a nossa label de CEP
     document.querySelector("#loadingCEP").classList.remove("oculto")
@@ -63,6 +120,10 @@ function preencheEndereco(endereco) {
 document.querySelector("form").addEventListener("submit", event => {
     event.preventDefault()
 
+    let button = document.querySelector('button[type="submit"]')
+    button.querySelector("span").classList.remove("oculto")
+    button.disabled = true
+
     let respostas = {}
     let campos = ["nome", "sobrenome", "email", "data_nascimento", "ddd", "telefone", "cep", "endereco", "bairro", "cidade", "estado"]
     
@@ -86,12 +147,15 @@ document.querySelector("form").addEventListener("submit", event => {
 
     let camposNumero = ["ddd", "telefone", "cep"]
     camposNumero.forEach(numero => respostas[numero] = Number(respostas[numero]))
+
+    let url = 'https://api-curso-programacao-web.vercel.app/api/usuarios'
+    if (usuario) url += `/${usuario.id}`
     
-    fetch("https://api-curso-programacao-web.vercel.app/api/usuarios", {
+    fetch(url, {
         headers: {
             "Authorization": "Basic " + btoa('admin:admin') //converte em base64
         },
-        method: "POST",
+        method: usuario ? "PUT" : "POST",
         body: JSON.stringify(respostas)
     })
         .then(resposta => {
@@ -99,6 +163,8 @@ document.querySelector("form").addEventListener("submit", event => {
             //caso a requisição retorne sucesso, o modal de sucesso é exibido
             if (resposta.ok) {
                 modalSucesso.show()
+                button.querySelector("span").classList.add("oculto")
+                button.disabled = false
             }
             //Caso não, lança um novo erro para ser capturado no catch
             else throw new Error("Erro na requisição!")
@@ -107,5 +173,7 @@ document.querySelector("form").addEventListener("submit", event => {
             console.log(error.message)
             //caso haja um erro na requisição, o modal de erro é exibido
             modal.show()
+            button.querySelector("span").classList.add("oculto")
+            button.disabled = false
         })
 })
--- projeto4/script.js
+++ projeto4/script.js
@@ -9,12 +9,30 @@ document.querySelector("#avisoDeErro").addEventListener('hidden.bs.modal', () =>
     document.querySelector("#loading").classList.add("oculto")
 })
 
+//Ativando o Bootstrap Modal para exclusão
+const modalDelete = new bootstrap.Modal('#deletar', {
+    keyboard: false,
+    backdrop: 'static'
+})
+
+//Ativando o Bootstrap Modal para sucesso
+const modalSucesso = new bootstrap.Modal('#sucesso', {
+    keyboard: false
+})
+
+//Fazendo a busca dos dados novamente quando o modal de sucesso for ocultado
+document.querySelector("#sucesso").addEventListener('hidden.bs.modal', buscarDados)
+
 //Ao iniciar a página, os dados serão buscados
 window.addEventListener("load", buscarDados)
 
+//Cria variavel para armanezar os usuarios
+var usuarios = []
+
 function buscarDados() {
-    //Oculta tabela e exibe o nosso loading
+    //Oculta tabela e gráficos e exibe o loading
     document.querySelector(".tabela").classList.add("oculto")
+    document.querySelector(".graficos").classList.add("oculto")
     document.querySelector("#loading").classList.remove("oculto")
 
     //Faz a requisição via fetch enviando a nossa credencial em base64 no header
@@ -26,8 +44,10 @@ function buscarDados() {
         .then(resposta => resposta.json()) // converte a resposta e, json
         .then(resposta => {
             console.log(resposta)
-            //chama a função de montar a tabela com os dados em json
-            montarTabela(resposta) 
+            //Armezena resposta em variavel
+            usuarios = resposta
+            //chama a função de exibir dados com os dados em json
+            exibirDados(resposta)
         })
         .catch(error => {
             console.log(error.message)
@@ -51,8 +71,18 @@ function montarTabela(usuarios) {
 
         //Monta nossa table row usando HTML e template string
         let linha = `<tr>
-            <td>${usuario.id}</td>
-            <td>${usuario.nome + " " + usuario.sobrenome}</td>
+            <td>
+                <button type="button" class="btn btn-dark" onclick="editar(this)">
+                    <i class="bi bi-pencil-fill"></i>
+                </button>
+            </td>
+            <td>
+                <button type="button" class="btn btn-dark" onclick="deletar(this)">
+                    <i class="bi bi-trash-fill"></i>
+                </button>
+            </td>
+            <td class="user-id">${usuario.id}</td>
+            <td class="user-name">${usuario.nome + " " + usuario.sobrenome}</td>
             <td>${usuario.email}</td>
             <td>${data_nascimento}</td>
             <td>${telefone}</td>
@@ -63,7 +93,182 @@ function montarTabela(usuarios) {
         tbody.innerHTML += linha
     })
 
-    //Exibe a tabela e oculta o loading
+    //Se o tbody estiver vazio, adiciona uma linha que preenche as 8 colunas exibindo mensagem de erro
+    if (!tbody.innerHTML) tbody.innerHTML = `<tr>
+        <td colspan="8">Não há dados</td>
+    </tr>`
+}
+
+function editar(button) {
+    //Busca o pai do button (que é o td) e depois o pai do td (que é o tr)
+    let linha = button.parentElement.parentElement
+    //Busca o id através da classe que demos a ele
+    let id = linha.querySelector(".user-id").textContent
+    console.log(id)
+
+    //Redireciona para a página do formulário passando o id como query params
+    window.location.href = "./formulario.html?id=" + id
+}
+
+function deletar(button) {
+    //Busca o pai do button (que é o td) e depois o pai do td (que é o tr)
+    let linha = button.parentElement.parentElement
+    //Busca o id e o nome através das classes que demos a eles
+    let id = linha.querySelector(".user-id").textContent
+    let name = linha.querySelector(".user-name").textContent
+    console.log(id)
+
+    //Prenche as informações no modal deletar
+    document.querySelector("#idUsuario").textContent = id
+    document.querySelector("#nomeUsuario").textContent = name
+
+    //Abre o modal deletar
+    modalDelete.show()
+}
+
+function excluir() {
+    //Exibe o loading do botão e oculta os mesmos
+    let modalDeletar = document.querySelector("#deletar")
+    modalDeletar.querySelector("span").classList.remove("oculto")
+    modalDeletar.querySelectorAll("button").forEach(button => button.disabled = true)
+
+    //Busca o id a ser excluido no modal deletar
+    let id = document.querySelector("#idUsuario").textContent
+
+    //Faz a requisição via fetch enviando a nossa credencial em base64 no header
+    //E informado o method DELETE
+    fetch("https://api-curso-programacao-web.vercel.app/api/usuarios/"+id, {
+        headers: {
+            "Authorization": "Basic " + btoa('admin:admin') //converte em base64
+        },
+        method: 'DELETE'
+    })
+        .then(resposta => {
+            console.log(resposta)
+            //Caso a requisição esteja ok, remove o loading e habilita os botões
+            //Também oculta o modal de deletar e abre o de sucesso
+            if (resposta.ok) {
+                modalDeletar.querySelector("span").classList.add("oculto")
+                modalDeletar.querySelectorAll("button").forEach(button => button.disabled = false)
+                modalDelete.hide()
+                modalSucesso.show()
+            }
+            //Caso não, lança um novo erro para ser capturado no catch
+            else throw new Error("Erro na requisição!")
+        })
+        .catch(error => {
+            console.log(error.message)
+            modalDeletar.querySelector("span").classList.add("oculto")
+            modalDeletar.querySelectorAll("button").forEach(button => button.disabled = false)
+            //Oculta o modal de deletar e abre o de erro
+            modalDelete.hide()
+            modal.show()
+        })
+}
+
+function filtrar() {
+    //Pega o valor do input
+    let value = document.querySelector("#filtro").value
+    //Cria expressão regular
+    var expressaoRegular = new RegExp(value, "i");
+    //Cria filtro usando o retorno do método test da expressão regular
+    let novoArray = usuarios.filter(usuario => {
+        let nomeCompleto = `${usuario.nome} ${usuario.sobrenome}`
+        return expressaoRegular.test(nomeCompleto)
+    })
+    //Chama função para exibir os novos dados
+    exibirDados(novoArray)
+}
+
+function exibirDados(dados) {
+    //Chama a função para montar a tabedla
+    montarTabela(dados)
+
+    const options = {
+        width: 500,
+        height: 350,
+        chartArea: { width: "100%", height: "80%" },
+    }
+
+    //Chama a função para criar o gráfico de cidades
+    const cidades = gerarDadosGraficos(dados, ['Cidade','Qtd Usuários'], 'cidade')
+    criarGrafico(cidades, { ...options, title: 'Cidade dos Usuários' }, 'grafico-cidades', 'column')
+
+    //Chama a função para criar o gráfico de estados
+    const estados = gerarDadosGraficos(dados, ['Estado','Qtd Usuários'], 'estado')
+    criarGrafico(estados, { ...options, title: 'Estado dos Usuários' }, 'grafico-estados', 'column')
+    
+    //Gera um array com os dados de provedores de e-mail
+    const provedoresEmail = dados.map(dado => {
+        let provedor = dado.email.split("@")[1].split(".")[0]
+        return {provedor: provedor}
+    })
+    //Chama a função para criar o gráfico de e-mails
+    const emails = gerarDadosGraficos(provedoresEmail, ['E-mail','Qtd Usuários'], 'provedor')
+    criarGrafico(emails, { ...options, title: 'E-mail dos Usuários' }, 'grafico-emails', 'pizza')
+
+    //Gera um array com os dados de idade do usuário
+    const idadesUsuarios = dados.map(dado => {
+        let idade = getIdade(dado.data_nascimento).toString()
+        return {idade: idade}
+    })
+    //Chama a função para criar o gráfico de idades
+    const idades = gerarDadosGraficos(idadesUsuarios, ['Idade','Qtd Usuários'], 'idade')
+    criarGrafico(idades, { ...options, title: 'Idades dos Usuários' }, 'grafico-idades', 'pizza')
+
+    //Exibe a tabela, a pesquisa, os gráficos e oculta o loading
+    document.querySelector(".pesquisa").classList.remove("oculto")
     document.querySelector(".tabela").classList.remove("oculto")
+    document.querySelector(".graficos").classList.remove("oculto")
     document.querySelector("#loading").classList.add("oculto")
+}
+
+function getIdade(data) {
+    //Puxa o timestamp de hoje
+    let hoje = new Date().getTime()
+    //Puxa o timestamp da data
+    let nascimento = new Date(data).getTime()
+    //Calcula a idade subtraindo o timestamp da data do de hoje
+    //Depois dividimos esses milisegundo pelo qtd de ms em um dia vezes 365.25 (1 ano)
+    let idade = (hoje - nascimento) / (86400000 * 365.25)
+    //Retorna a idade arredondando para baixo 
+    return Math.floor(idade)
+}
+
+//Carrega os gráficos da biblioteca em sua versão atual
+google.charts.load('current', {'packages':['corechart']});
+
+function criarGrafico(dados, options, id, tipo) {
+    //Encontra elemento
+    var elemento = document.getElementById(id)
+    //Caso não haja dados e seja um gráfico de coluna, é necessário a primeira linha
+    if (dados.length === 1 && tipo == "column") dados.push([null,0])
+    //Cria visualização de dados do google através de array
+    var data = google.visualization.arrayToDataTable(dados)
+
+    //Cria o gráfico de acordo com o parametro tipo
+    var chart
+    if (tipo == "column") chart = new google.visualization.ColumnChart(elemento)
+    else if (tipo == "pizza") chart = new google.visualization.PieChart(elemento)
+
+    //Renderiza o gráfico
+    chart.draw(data, options);
+}
+
+function gerarDadosGraficos(usuarios, header, campo) {
+    //O array inicia com a linha do header
+    var array = [header]
+    //Busca os dados do campo informado
+    var lista = usuarios.map(usuario => usuario[campo])
+    //Retona uma lista com valores unicos (sem repetições)
+    var valores = [...new Set(lista)]
+    //Realiza a contagem percorrendo o array lista e insere a nova linha ao array
+    valores.forEach(valor => {
+        let contagem = 0
+        lista.forEach(dado => {
+            if (dado == valor) contagem++
+        })
+        array.push([valor,contagem])
+    })
+    return array
 }
